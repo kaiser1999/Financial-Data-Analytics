84776952c9d703a84feb8b28dc3eedd47c42d2fa
Kaiser Fan <kaiser3@hotmail.com.hk>
2023-12-05 17:45:54 +0800

    Update

--- 05 Numerical Finance/Financial Derivatives.R
+++ 05 Numerical Finance/Financial Derivatives.R
@@ -1,70 +1,121 @@
 # Initialize variables
-S_0 <- 10; q <- 0; sigma <- 0.7; r <- 0; T <- 1; K <- 9; h <- 0.3
+S_0 <- 10; q <- 0; sigma <- 0.7; r <- 0; T <- 1; K <- 8; h <- 0.3
 params <- list(mean=(r-q-sigma^2/2)*T, sd=sigma*sqrt(T))
+params_minus_h<-list(mean=(r-q-(sigma-h)^2/2)*T,sd=(sigma-h)*sqrt(T))
+params_plus_h <-list(mean=(r-q-(sigma+h)^2/2)*T,sd=(sigma+h)*sqrt(T))
 discount <- exp(-r * T)
 
-# Compute the exact Delta
+# Compute the exact Delta and Vega
 d_plus <- (log(S_0/K) + (r-q+sigma^2/2)*T) / (sigma*sqrt(T))
-(delta <- pnorm(d_plus))
+(exact_delta <- pnorm(d_plus))
+(exact_vega <- S_0 * sqrt(T) * dnorm(d_plus))
 
 ################################################################################
 set.seed(4002)
 # Create a data frame to store results
-results <- data.frame(h=numeric(0), n=numeric(0), delta=numeric(0),
-                      est_forward_diff=numeric(0), 
-                      est_central_diff=numeric(0))
+results <- data.frame(n=numeric(0), exact_delta=numeric(0), 
+                      exact_vega=numeric(0), 
+                      est_delta_forward=numeric(0), 
+                      est_vega_forward=numeric(0), 
+                      est_delta_central=numeric(0), 
+                      est_vega_central=numeric(0))
 
 n_size <- c(1e6, 1e8)
-# Estimate Delta by forward and central difference with different n
+# Estimate Delta and Vega using forward and central difference 
+# with different n
 for (n in n_size) {
   # Generate the Black-Scholes sample
   S_T <- S_0 * exp(do.call(rnorm, c(n, params)))
-  Y_bar_S0 <- discount * mean(pmax(S_T - K, 0))
   
+  # Estimate Delta using forward and central difference method
+  Y_bar_S0 <- discount * mean(pmax(S_T - K, 0))
   S_T_minus_h <- (S_0 - h) * exp(do.call(rnorm, c(n, params)))
   Y_bar_S0_minus_h <- discount * mean(pmax(S_T_minus_h - K, 0))
-  
   S_T_plus_h <- (S_0 + h) * exp(do.call(rnorm, c(n, params)))
   Y_bar_S0_plus_h <- discount * mean(pmax(S_T_plus_h - K, 0))
   
-  # Estimate using forward difference method
-  est_forward_diff <- (Y_bar_S0_plus_h - Y_bar_S0) / h
+  est_delta_forward <- (Y_bar_S0_plus_h - Y_bar_S0) / h
+  est_delta_central<- (Y_bar_S0_plus_h - Y_bar_S0_minus_h) / (2*h)
   
-  # Estimate using central difference method
-  est_central_diff <- (Y_bar_S0_plus_h - Y_bar_S0_minus_h) / (2*h)
+  # Estimate Vega using forward and central difference method
+  Y_bar_sigma <- Y_bar_S0
+  S_T_minus_h <- S_0  * exp(do.call(rnorm, c(n, params_minus_h)))
+  Y_bar_sigma_minus_h <- discount * mean(pmax(S_T_minus_h - K, 0))
+  S_T_plus_h <- S_0  * exp(do.call(rnorm, c(n, params_plus_h)))
+  Y_bar_sigma_plus_h <- discount * mean(pmax(S_T_plus_h - K, 0))
+  
+  est_vega_forward <- (Y_bar_sigma_plus_h - Y_bar_sigma)/h
+  est_vega_central<- (Y_bar_sigma_plus_h - Y_bar_sigma_minus_h)/(2*h)
   
   # Store the results in the data frame
-  output <- data.frame(h=h, n=n, delta=delta, 
-                       est_forward_diff=est_forward_diff, 
-                       est_central_diff=est_central_diff)
+  output <- data.frame(h=h, n=n, exact_delta=exact_delta, 
+                       est_delta_forward=est_delta_forward, 
+                       est_delta_central=est_delta_central,
+                       exact_vega=exact_vega,
+                       est_vega_forward=est_vega_forward, 
+                       est_vega_central=est_vega_central)
   results <- rbind(results, output)
 }
 
-results
+################################################################################
+# Subset results for Delta 
+(delta_results <- results[, c(1,2,3,4,5)])
 
-# Relative error for forward and central difference methods
-abs(results$est_forward_diff - delta) / delta * 100
-abs(results$est_central_diff - delta) / delta * 100
+# Relative error 
+abs(results$est_delta_forward - exact_delta) / exact_delta * 100
+abs(results$est_delta_central - exact_delta) / exact_delta * 100
+
+# Subset results for Vega
+(vega_results <- results[, c(1,2,6,7,8)])
+
+# Relative error 
+abs(results$est_vega_forward - exact_vega) / exact_vega * 100
+abs(results$est_vega_central - exact_vega) / exact_vega * 100
 
 ################################################################################
 set.seed(4002)
 
-n_size <- seq(400, 20000, by=400)
-est_pathwise <- c()
+n_size <- seq(400, 30000, by=400)
+est_delta_pathwise <- c(); est_vega_pathwise <- c()
+est_delta_lr <- c(); est_vega_lr <- c()
 
-# Estimate Delta by pathwise method with different n
+# Estimate Delta using pathwise method with different n
 for (i in 1:length(n_size)) {
   # Generate the Black-Scholes sample
   S_T <- S_0 * exp(do.call(rnorm, c(n_size[i], params)))
-
-  # Estimate using pathwise method
+  
+  # Estimate Delta using pathwise method
   result <- (S_T > K) * S_T / S_0
-  est_pathwise[i] <- discount * mean(result)
+  est_delta_pathwise[i] <- discount * mean(result)
+  
+  # Estimate Vega using pathwise method
+  result <- (S_T > K)*S_T*(log(S_T/S_0) - (r - q + sigma^2/2)*T)
+  est_vega_pathwise[i] <- discount / sigma * mean(result)
+  
+  # Estimate Delta using likelihood ratio method
+  h <- (log(S_T/S_0) - ((r - sigma^2/2)*T)) / (sigma*sqrt(T))
+  result <- pmax(S_T - K, 0) * h / (S_0 * sigma * sqrt(T))
+  est_delta_lr[i] <- mean(discount * result)
+  
+  # Estimate Vega using likelihood ratio method
+  result <- pmax(S_T - K, 0) * (-1/sigma + h^2/sigma -sqrt(T)*h)
+  est_vega_lr[i] <- mean(discount * result)
 }
 
 # Plot the graph of estimated Delta and exact Delta
-plot(n_size, est_pathwise, type="o", col="red", ylim=c(0.4, 0.9), 
+plot(n_size, est_delta_pathwise, type="o", col="red", ylim=c(0.5, 1),
      xlab="number of paths", ylab="Delta", main="", lwd=1.5, pch=21)
-abline(h=delta, col="blue", lwd=1.5)
-legend("topright", legend=c("pathwise estimate", "exact"), 
-       col=c("red","blue"), lwd=1.5, pch=c(21, NA))
+lines(n_size, est_delta_lr, type="o", col="black", lwd=1.5, pch=21)
+abline(h=exact_delta, col="blue", lwd=1.8)
+legend("topright", col=c("red", "black", "blue"), lwd=1.5,
+       legend=c("pathwise estimate", "likelihood ratio estimate", 
+                "exact"), pch=c(21, 21, NA))
+
+# Plot the graph of estimated Vega and exact Vega
+plot(n_size, est_vega_pathwise, type="o", col="red", ylim=c(2, 4.5),
+     xlab="number of paths", ylab="Vega", main="", lwd=1.5, pch=21)
+lines(n_size, est_vega_lr, type="o", col="black", lwd=1.5, pch=21)
+abline(h=exact_vega, col="blue", lwd=1.8)
+legend("topright", col=c("red", "black", "blue"), lwd=1.5,
+       legend=c("pathwise estimate", "likelihood ratio estimate", 
+                "exact"), pch=c(21, 21, NA))
--- 05 Numerical Finance/Financial Derivatives.py
+++ 05 Numerical Finance/Financial Derivatives.py
@@ -3,78 +3,134 @@ import pandas as pd
 from scipy.stats import norm
 
 # Initialize variables
-S_0 = 10; q = 0; sigma = 0.7; r = 0; T = 1; K = 9; h = 0.3
+S_0 = 10; q = 0; sigma = 0.7; r = 0; T = 1; K = 8; h = 0.3
 params = {"loc": (r-q-sigma**2/2)*T, "scale": sigma*np.sqrt(T)}
+params_minus_h = {"loc": (r-q-(sigma-h)**2/2)*T, 
+                  "scale": (sigma-h)*np.sqrt(T)}
+params_plus_h  = {"loc": (r-q-(sigma+h)**2/2)*T, 
+                 "scale": (sigma+h)*np.sqrt(T)}
 discount = np.exp(-r * T)
 
 # Compute the exact Delta
 d_plus = (np.log(S_0/K) + (r-q+sigma**2/2)*T) / (sigma*np.sqrt(T))
 delta = norm.cdf(d_plus)
+vega = S_0 * np.sqrt(T) * norm.pdf(d_plus)
 print(delta)
+print(vega)
 
 #%%
 np.random.seed(4012)
 
 # Create a data frame to store results
 results = pd.DataFrame(columns=["h", "n", "delta", 
-                                "est_forward_diff", 
-                                "est_central_diff"])
+                                "est_forward_delta", 
+                                "est_central_delta",
+                                "vega", "est_forward_vega", 
+                                "est_central_vega"])
 n_size = [1e6, 1e8]
 # Estimate Delta by forward and central difference with different n
 for n in n_size:
     # Generate the Black-Scholes sample
     S_T = S_0 * np.exp(norm.rvs(**params, size=int(n)))
     Y_bar_S0 = discount * np.mean(np.maximum(S_T - K, 0))
-  
+    
+    # Estimate delta using forward and central difference method
     S_T_minus_h = (S_0 - h) * np.exp(norm.rvs(**params, size=int(n)))
-    Y_bar_S0_minus_h = discount * np.mean(np.maximum(S_T_minus_h - K, 0))
-  
+    Y_bar_S0_minus_h = discount * np.mean(np.maximum(S_T_minus_h-K, 0))
     S_T_plus_h = (S_0 + h) * np.exp(norm.rvs(**params, size=int(n)))
     Y_bar_S0_plus_h = discount * np.mean(np.maximum(S_T_plus_h - K, 0))
-  
-    # Estimate using forward difference method
-    est_forward_diff = (Y_bar_S0_plus_h - Y_bar_S0) / h
-  
-    # Estimate using central difference method
-    est_central_diff = (Y_bar_S0_plus_h - Y_bar_S0_minus_h) / (2*h)
+    
+    est_forward_delta = (Y_bar_S0_plus_h - Y_bar_S0) / h
+    est_central_delta = (Y_bar_S0_plus_h - Y_bar_S0_minus_h) / (2*h)
+    
+    # Estimate vega using forward and central difference method
+    Y_T_minus_h = S_0 * np.exp(norm.rvs(**params_minus_h, size=int(n)))
+    Y_bar_sigma_minus_h = discount*np.mean(np.maximum(Y_T_minus_h-K,0))
+    
+    Y_T_plus_h = S_0 * np.exp(norm.rvs(**params_plus_h, size=int(n)))
+    Y_bar_sigma_plus_h = discount * np.mean(np.maximum(Y_T_plus_h-K,0))
+    
+    est_forward_vega = (Y_bar_sigma_plus_h - Y_bar_S0) / h
+    est_central_vega = (Y_bar_sigma_plus_h - Y_bar_sigma_minus_h)/(2*h)
   
     # Store the results in the data frame
     output = pd.DataFrame({"h": [h], "n": [int(n)], "delta": [delta], 
-                           "est_forward_diff": [est_forward_diff], 
-                           "est_central_diff": [est_central_diff]})
+                           "est_forward_delta": [est_forward_delta], 
+                           "est_central_delta": [est_central_delta],
+                           "vega": [vega],
+                           "est_forward_vega": [est_forward_vega], 
+                           "est_central_vega": [est_central_vega]})
     results = pd.concat([results, output])
 
-print(results)
+#%%
+# Print the result of delta and vega
+print(results[["h", "n", "delta", "est_forward_delta",
+               "est_central_delta"]])
+print(results[["h", "n", "vega", "est_forward_vega",
+               "est_central_vega"]])
 
 # Relative error for forward and central difference methods
-print(abs(results.est_forward_diff.values - delta) / delta * 100)
-print(abs(results.est_central_diff.values - delta) / delta * 100)
+print(abs(results.est_forward_delta.values - delta) / delta * 100)
+print(abs(results.est_central_delta.values - delta) / delta * 100)
+print(abs(results.est_forward_vega.values - vega) / vega * 100)
+print(abs(results.est_central_vega.values - vega) / vega * 100)
 
 #%%
 np.random.seed(4012)
-
-n_size = np.arange(400, 20000, 400)
-est_pathwise = []
+n_size = np.arange(400, 30000, 400)
+est_pathwise_delta, est_pathwise_vega = [], []
+est_lr_delta, est_lr_vega = [], []
 
 # Estimate Delta by pathwise method with different n
 for i in range(len(n_size)):
     # Generate the Black-Scholes sample
     S_T = S_0 * np.exp(norm.rvs(**params, size=int(n_size[i])))
     
-    # Estimate using pathwise method
+    # Estimate Delta using pathwise method
     result = (S_T > K) * S_T / S_0
-    est_pathwise.append(discount * np.mean(result))
-
+    est_pathwise_delta.append(discount * np.mean(result))
+    
+    # Estimate Vega using pathwise method
+    result = (S_T > K) * S_T *(np.log(S_T/S_0)-(r-q+sigma**2/2)*T)
+    est_pathwise_vega.append(discount / sigma * np.mean(result))
+    
+    # Estimate Delta using likelihood ratio method
+    h = (np.log(S_T/ S_0) - ((r - sigma**2/2)*T)) / (sigma*np.sqrt(T))
+    result = np.maximum(S_T - K, 0) * h / (S_0 * sigma * np.sqrt(T))
+    est_lr_delta.append( discount * np.mean(result))
+  
+    # Estimate Vega using likelihood ratio method
+    result = np.maximum(S_T-K, 0)*(-1/sigma +h**2/sigma -np.sqrt(T)*h)
+    est_lr_vega.append(discount * np.mean(result))
+    
 import matplotlib.pyplot as plt
-plt.figure(figsize=(10, 7))
+
 # Plot the graph of estimated Delta and exact Delta
-plt.plot(n_size, est_pathwise, "ro-", markerfacecolor='white',
-         linewidth=1.5, markersize=10, label="pairwise estimate")
-plt.ylim((0.4, 0.9))
+plt.figure(figsize=(10, 7))
+plt.plot(n_size, est_pathwise_delta, "ro-", markersize=7, 
+         linewidth=1.5, label="pathwise estimate")
+plt.plot(n_size, est_lr_delta, "ko-", markersize=7, linewidth=1.5, 
+         label="likelihood ratio estimate")
+plt.axhline(y=delta, color='blue', linewidth=1.8, label="exact")
+plt.ylim((0.5, 1))
 plt.xlabel("number of paths", fontsize=15)
 plt.ylabel("Delta", fontsize=15)
-plt.axhline(y=delta, color='b', linestyle='-', label="exact")
 plt.legend(fontsize=15)
 
 plt.tight_layout()
-plt.savefig("../Picture/Pathwise_estimate.png", dpi=200)
+plt.savefig("likelihood_delta.png", dpi=200)
+
+# Plot the graph of estimated Vega and exact Vega
+plt.figure(figsize=(10, 7))
+plt.plot(n_size, est_pathwise_vega, "ro-", markersize=7, linewidth=1.5, 
+         label="pathwise estimate")
+plt.plot(n_size, est_lr_vega, "ko-", markersize=7, linewidth=1.5, 
+         label="likelihood ratio estimate")
+plt.axhline(y=vega, color='blue', linewidth=1.8, label="exact")
+plt.ylim((1.5, 4.5))
+plt.xlabel("number of paths", fontsize=15)
+plt.ylabel("Vega", fontsize=15)
+plt.legend(fontsize=15)
+
+plt.tight_layout()
+plt.savefig("likelihood_vega.png", dpi=200)
