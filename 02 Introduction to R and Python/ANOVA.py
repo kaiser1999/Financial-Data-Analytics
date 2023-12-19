399a4f11bc452cd35c436132cd080d2805b9ad7d
Kaiser Fan <kaiser3@hotmail.com.hk>
2023-12-19 22:04:16 +0000

    Update

--- 02 Introduction to R and Python/ANOVA.py
+++ 02 Introduction to R and Python/ANOVA.py
@@ -30,14 +30,16 @@ def Remove_Outlier(index, outlier_factor=1.5):
 
 # Hyperparameter
 outlier_removal = True
-start_year = 2011
-end_year = 2020
-
 if outlier_removal:
     SPX = Remove_Outlier(SPX)
     HSI = Remove_Outlier(HSI)
     FTSE = Remove_Outlier(FTSE)
 
+#%%
+### from 2005 to 2007 ###
+#%%
+start_year = 2005
+end_year = 2007
 year_name = " Year: " + str(start_year) + "-" + str(end_year)
 Chosen_Year = range(start_year, end_year + 1)
 Chosen_SPX = SPX[SPX.Year.isin(Chosen_Year)]
@@ -52,17 +54,21 @@ ax1.hist(Chosen_SPX.log_return, bins="sturges", ec='black')
 ax2.hist(Chosen_HSI.log_return, bins="sturges", ec='black')
 ax3.hist(Chosen_FTSE.log_return, bins="sturges", ec='black')
 plt.tight_layout()
-fig.savefig("../Graphs/Index log_return histogram.png", dpi=200)
+fig.savefig(f"../Picture/Index log_return histogram {start_year}_{end_year}.png", dpi=200)
 
-#%%
 import scipy.stats as stats
-import seaborn as sns
-from statsmodels.stats.multicomp import MultiComparison
 
 u1, u2, u3 = Chosen_SPX.log_return, Chosen_HSI.log_return, Chosen_FTSE.log_return
-print(stats.kstest(u1, 'norm', args=(np.mean(u1), np.std(u1))))
-print(stats.kstest(u2, 'norm', args=(np.mean(u2), np.std(u2))))
-print(stats.kstest(u3, 'norm', args=(np.mean(u3), np.std(u3))))
+print(stats.kstest(u1, 'norm', args=(np.mean(u1), np.std(u1, ddof=1)), 
+                   method="asymp"))
+print(stats.kstest(u2, 'norm', args=(np.mean(u2), np.std(u2, ddof=1)), 
+                   method="asymp"))
+print(stats.kstest(u3, 'norm', args=(np.mean(u3), np.std(u3, ddof=1)), 
+                   method="asymp"))
+
+#%%
+import seaborn as sns
+from statsmodels.stats.multicomp import MultiComparison
 
 fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(22,7))
 plt.subplots_adjust(wspace=0.4)      # horizontal spacing between plots
@@ -75,8 +81,7 @@ ax1.boxplot([Chosen_SPX.log_return, Chosen_HSI.log_return, Chosen_FTSE.log_retur
 Chosen_SPX.loc[:, "Index"] = "SPX"
 Chosen_HSI.loc[:, "Index"] = "HSI"
 Chosen_FTSE.loc[:, "Index"] = "FTSE"
-
-AllIndex = Chosen_SPX.append([Chosen_HSI, Chosen_FTSE])
+AllIndex = pd.concat([Chosen_SPX, Chosen_HSI, Chosen_FTSE])
 
 sns.pointplot(x='Index', y="log_return", data=AllIndex, ci=95, ax=ax2)
 ax2.set_title("Mean Plot with 95% CI." + year_name, fontsize=20)
@@ -98,7 +103,7 @@ ax3.title.set_fontsize(20)
 ax3.set_xlabel("Daily log-return", fontsize=15)
 ax3.set_ylabel("Index", fontsize=15)
 plt.tight_layout()
-fig.savefig("../Graphs/Boxplot, Meanplot, Tukey HSD.png", dpi=200)
+fig.savefig(f"../Picture/Boxplot, Meanplot, Tukey HSD {start_year}_{end_year}.png", dpi=200)
 
 #%%
 mu_SPX = np.mean(Chosen_SPX.log_return)
@@ -130,6 +135,110 @@ FTSE_vs_HSI = np.abs(mu_FTSE - mu_HSI) / FTSE_HSI_SE_ANOVA
 
 from statsmodels.stats.libqsturng import psturng
 
-print(psturng(q=SPX_vs_HSI, r=3, v=df)[0])
-print(psturng(q=FTSE_vs_SPX, r=3, v=df)[0])
+print(psturng(q=SPX_vs_HSI, r=3, v=df))
+print(psturng(q=FTSE_vs_SPX, r=3, v=df))
+print(psturng(q=FTSE_vs_HSI, r=3, v=df))
+
+#%%
+### from 2018 to 2020 ###
+#%%
+start_year = 2018
+end_year = 2020
+year_name = " Year: " + str(start_year) + "-" + str(end_year)
+Chosen_Year = range(start_year, end_year + 1)
+Chosen_SPX = SPX[SPX.Year.isin(Chosen_Year)]
+Chosen_HSI = HSI[HSI.Year.isin(Chosen_Year)]
+Chosen_FTSE = FTSE[FTSE.Year.isin(Chosen_Year)]
+
+fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(20,7))
+ax1.set_title("SPX daily log-return." + year_name)
+ax2.set_title("HSI daily log-return." + year_name)
+ax3.set_title("FTSE daily log-return." + year_name)
+ax1.hist(Chosen_SPX.log_return, bins="sturges", ec='black')
+ax2.hist(Chosen_HSI.log_return, bins="sturges", ec='black')
+ax3.hist(Chosen_FTSE.log_return, bins="sturges", ec='black')
+plt.tight_layout()
+fig.savefig(f"../Picture/Index log_return histogram {start_year}_{end_year}.png", dpi=200)
+
+import scipy.stats as stats
+
+u1, u2, u3 = Chosen_SPX.log_return, Chosen_HSI.log_return, Chosen_FTSE.log_return
+print(stats.kstest(u1, 'norm', args=(np.mean(u1), np.std(u1, ddof=1)), 
+                   method="asymp"))
+print(stats.kstest(u2, 'norm', args=(np.mean(u2), np.std(u2, ddof=1)), 
+                   method="asymp"))
+print(stats.kstest(u3, 'norm', args=(np.mean(u3), np.std(u3, ddof=1)), 
+                   method="asymp"))
+
+#%%
+import seaborn as sns
+from statsmodels.stats.multicomp import MultiComparison
+
+fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(22,7))
+plt.subplots_adjust(wspace=0.4)      # horizontal spacing between plots
+ax1.set_title("Boxplot." + year_name, fontsize=20)
+ax1.set_xlabel("Location", fontsize=15)
+ax1.set_ylabel("Daily log-return", fontsize=15)
+ax1.boxplot([Chosen_SPX.log_return, Chosen_HSI.log_return, Chosen_FTSE.log_return],
+            widths=0.7, labels=["SPX", "HSI", "FTSE"])
+
+Chosen_SPX.loc[:, "Index"] = "SPX"
+Chosen_HSI.loc[:, "Index"] = "HSI"
+Chosen_FTSE.loc[:, "Index"] = "FTSE"
+AllIndex = pd.concat([Chosen_SPX, Chosen_HSI, Chosen_FTSE])
+
+sns.pointplot(x='Index', y="log_return", data=AllIndex, ci=95, ax=ax2)
+ax2.set_title("Mean Plot with 95% CI." + year_name, fontsize=20)
+ax2.set_xlabel("Index", fontsize=15)
+ax2.set_ylabel("Daily log-return", fontsize=15)
+
+F, p = stats.f_oneway(Chosen_SPX.log_return, Chosen_HSI.log_return, 
+                      Chosen_FTSE.log_return)
+# Reject the null hypothesis?
+print(p < 0.05)
+
+comp = MultiComparison(data=AllIndex.log_return, groups=AllIndex.Index, 
+                       group_order=["SPX", "HSI", "FTSE"])
+TurkeyHSD_result = comp.tukeyhsd()
+print(TurkeyHSD_result)
+
+TurkeyHSD_result.plot_simultaneous(figsize=(22, 7), ax=ax3)
+ax3.title.set_fontsize(20)
+ax3.set_xlabel("Daily log-return", fontsize=15)
+ax3.set_ylabel("Index", fontsize=15)
+plt.tight_layout()
+fig.savefig(f"../Picture/Boxplot, Meanplot, Tukey HSD {start_year}_{end_year}.png", dpi=200)
+
+#%%
+mu_SPX = np.mean(Chosen_SPX.log_return)
+mu_HSI = np.mean(Chosen_HSI.log_return)
+mu_FTSE = np.mean(Chosen_FTSE.log_return)
+
+SSE_SPX = np.sum((Chosen_SPX.log_return - mu_SPX)**2)
+SSE_HSI = np.sum((Chosen_HSI.log_return - mu_HSI)**2)
+SSE_FTSE = np.sum((Chosen_FTSE.log_return - mu_FTSE)**2)
+
+n_SPX = len(Chosen_SPX.log_return)
+n_HSI = len(Chosen_HSI.log_return)
+n_FTSE = len(Chosen_FTSE.log_return)
+
+df = len(AllIndex.log_return) - 3
+print(df)
+
+Within_group_MSE = (SSE_SPX + SSE_HSI + SSE_FTSE) / df
+print(Within_group_MSE)
+
+# Harmonic mean used for the two sample sizes
+SPX_HSI_SE_ANOVA = np.sqrt(Within_group_MSE / (2/(1/n_SPX + 1/n_HSI)))
+FTSE_SPX_SE_ANOVA = np.sqrt(Within_group_MSE / (2/(1/n_FTSE + 1/n_SPX)))
+FTSE_HSI_SE_ANOVA = np.sqrt(Within_group_MSE / (2/(1/n_FTSE + 1/n_HSI)))
+
+SPX_vs_HSI = np.abs(mu_SPX - mu_HSI) / SPX_HSI_SE_ANOVA
+FTSE_vs_SPX = np.abs(mu_FTSE - mu_SPX) / FTSE_SPX_SE_ANOVA
+FTSE_vs_HSI = np.abs(mu_FTSE - mu_HSI) / FTSE_HSI_SE_ANOVA
+
+from statsmodels.stats.libqsturng import psturng
+
+print(psturng(q=SPX_vs_HSI, r=3, v=df))
+print(psturng(q=FTSE_vs_SPX, r=3, v=df))
 print(psturng(q=FTSE_vs_HSI, r=3, v=df))
--- 02 Introduction to R and Python/Anova.R
+++ 02 Introduction to R and Python/Anova.R
@@ -1,6 +1,7 @@
 #Set directory: Run this on source instead of Console!!
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
+################################################################################
 SPX <- read.csv("../Datasets/SPX.csv", header=TRUE, row.names=1)
 HSI <- read.csv("../Datasets/HSI.csv", header=TRUE, row.names=1)
 FTSE <- read.csv("../Datasets/FTSE.csv", header=TRUE, row.names=1)
@@ -30,14 +31,17 @@ Remove_Outlier <- function(index, outlier_factor=1.5){
 
 # Hyperparameter
 outlier_removal <- TRUE
-start_year <- 2005
-end_year <- 2007
-
 if (outlier_removal){
   SPX <- Remove_Outlier(SPX)
   HSI <- Remove_Outlier(HSI)
   FTSE <- Remove_Outlier(FTSE)
 }
+
+################################################################################
+### from 2005 to 2007 ###
+################################################################################
+start_year <- 2005
+end_year <- 2007
 
 year_name <- paste0(" Year: ", start_year, "-", end_year)
 Chosen_Year <- start_year:end_year
@@ -59,6 +63,85 @@ ks.test(u1, y=pnorm, mean=mean(u1), sd=sd(u1))
 ks.test(u2, y=pnorm, mean=mean(u2), sd=sd(u2))
 ks.test(u3, y=pnorm, mean=mean(u3), sd=sd(u3))
 
+################################################################################
+par(mfrow=c(1,3))
+boxplot(Chosen_SPX$log_return, Chosen_HSI$log_return, Chosen_FTSE$log_return, 
+        names=c("SPX", "HSI", "FTSE"), xlab="Index", ylab="Daily log-return",
+        frame=FALSE, col=c("#00AFBB", "#E7B800", "#FC4E07"), boxwex=0.75,
+        main=paste0("Boxplot.", year_name))
+
+Chosen_SPX$Index <- "SPX"
+Chosen_HSI$Index <- "HSI"
+Chosen_FTSE$Index <- "FTSE"
+AllIndex <- rbind(Chosen_SPX, Chosen_HSI, Chosen_FTSE)
+AllIndex$Index <- factor(AllIndex$Index, c("SPX", "HSI", "FTSE"))
+
+library("gplots")
+plotmeans(log_return~Index, data=AllIndex, xlab="Index", ylab="Daily log-return",
+          main=paste0("Mean Plot with 95% CI.", year_name))
+anova.test <- aov(log_return~Index, data=AllIndex)
+summary(anova.test)
+# Reject the null hypothesis?
+summary(anova.test)[[1]][[1, "Pr(>F)"]] < 0.05
+
+(tukey.test <- TukeyHSD(anova.test))
+plot(tukey.test)
+
+mu_SPX <- mean(Chosen_SPX$log_return)
+mu_HSI <- mean(Chosen_HSI$log_return)
+mu_FTSE <- mean(Chosen_FTSE$log_return)
+
+SSE_SPX <- sum((Chosen_SPX$log_return - mu_SPX)^2)
+SSE_HSI <- sum((Chosen_HSI$log_return - mu_HSI)^2)
+SSE_FTSE <- sum((Chosen_FTSE$log_return - mu_FTSE)^2)
+
+n_SPX <- length(Chosen_SPX$log_return)
+n_HSI <- length(Chosen_HSI$log_return)
+n_FTSE <- length(Chosen_FTSE$log_return)
+
+(df <- length(AllIndex$log_return) - 3)
+(Within_group_MSE <- (SSE_SPX + SSE_HSI + SSE_FTSE) / df)
+
+# Harmonic mean used for the two sample sizes
+SPX_HSI_SE_ANOVA <- sqrt(Within_group_MSE / (2/(1/n_SPX + 1/n_HSI)))
+FTSE_SPX_SE_ANOVA <- sqrt(Within_group_MSE / (2/(1/n_FTSE + 1/n_SPX)))
+FTSE_HSI_SE_ANOVA <- sqrt(Within_group_MSE / (2/(1/n_FTSE + 1/n_HSI)))
+
+SPX_vs_HSI <- abs(mu_SPX - mu_HSI) / SPX_HSI_SE_ANOVA
+FTSE_vs_SPX <- abs(mu_FTSE - mu_SPX) / FTSE_SPX_SE_ANOVA
+FTSE_vs_HSI <- abs(mu_FTSE - mu_HSI) / FTSE_HSI_SE_ANOVA
+
+1 - ptukey(q=SPX_vs_HSI, nmeans=3, df=df)
+1 - ptukey(q=FTSE_vs_SPX, nmeans=3, df=df)
+1 - ptukey(q=FTSE_vs_HSI, nmeans=3, df=df)
+
+################################################################################
+### from 2018 to 2020 ###
+################################################################################
+start_year <- 2018
+end_year <- 2020
+
+year_name <- paste0(" Year: ", start_year, "-", end_year)
+Chosen_Year <- start_year:end_year
+Chosen_SPX <- SPX[SPX$Year %in% Chosen_Year,]
+Chosen_HSI <- HSI[HSI$Year %in% Chosen_Year,]
+Chosen_FTSE <- FTSE[FTSE$Year %in% Chosen_Year,]
+
+par(mfrow=c(1,3))
+hist(Chosen_SPX$log_return, breaks=20, xlab="SPX", cex.lab=1.5, cex.axis=1.5,
+     cex.main=1.5, main=paste0("SPX daily log-return.", year_name))
+hist(Chosen_HSI$log_return, breaks=20, xlab="HSI", cex.lab=1.5, cex.axis=1.5,
+     cex.main=1.5, main=paste0("HSI daily log-return.", year_name))
+hist(Chosen_FTSE$log_return, breaks=20, xlab="FTSE", cex.lab=1.5, cex.axis=1.5,
+     cex.main=1.5, main=paste0("FTSE daily log-return.", year_name))
+
+u1 <- Chosen_SPX$log_return; u2 <- Chosen_HSI$log_return
+u3 <- Chosen_FTSE$log_return
+ks.test(u1, y=pnorm, mean=mean(u1), sd=sd(u1))
+ks.test(u2, y=pnorm, mean=mean(u2), sd=sd(u2))
+ks.test(u3, y=pnorm, mean=mean(u3), sd=sd(u3))
+
+################################################################################
 par(mfrow=c(1,3))
 boxplot(Chosen_SPX$log_return, Chosen_HSI$log_return, Chosen_FTSE$log_return, 
         names=c("SPX", "HSI", "FTSE"), xlab="Index", ylab="Daily log-return",
