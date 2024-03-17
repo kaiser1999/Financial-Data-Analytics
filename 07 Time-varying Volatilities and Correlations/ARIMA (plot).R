3f5932c91a20649223562cb5b9b917b3d1718735
Kaiser Fan <kaiser3@hotmail.com.hk>
2024-03-17 21:24:11 +0000

    Delete

--- 07 Time-varying Volatilities and Correlations/ARIMA (plot).R
+++
@@ -1,8 +0,0 @@
-set.seed(4002)
-AR_2 <- arima.sim(n=200, list(ar=c(0.2897, 0.4858)), sd=1)
-set.seed(4002)
-MA_2 <- arima.sim(n=200, list(ma=c(0.1897, 0.4858)), sd=1)
-
-par(mfrow=c(1,2))
-acf(AR_2); pacf(AR_2)
-acf(MA_2); pacf(MA_2)
